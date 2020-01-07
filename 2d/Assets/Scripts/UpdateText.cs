1e619a2795a75d87f21322a1a6c873e160524591
Rhizling <saisakhamuri10@gmail.com>
2020-01-06 20:08:44 -0500

    cee

--- 2d/Assets/Scripts/UpdateText.cs
+++ 2d/Assets/Scripts/UpdateText.cs
@@ -19,27 +19,27 @@ public class UpdateText : MonoBehaviour
         if (PermanentUI.perm.LastScene == 0)
         {
             Description.text = "";
-            Requirement.text = "The Objective of the Next Level is to collect 75 coins within 90 seconds";
+            Requirement.text = "Service: March of Dimes needs your help fundraising! Collect 75 coins in 90 seconds";
             Current.text = "";
             PermanentUI.perm.Reset();
         }
         else if (PermanentUI.perm.LastScene == 1)
         {
-            Description.text = "The Objective of the Next Level is to Answer All the Questions Correctly within 90 seconds";
+            Description.text = "Education: Answer 5 questions in 90 seconds to learn about FBLA's Business Achievement Awards!";
             Requirement.text = "The required coins to access the next level is 75";
             Current.text = "You have " + PermanentUI.perm.points +" points";
             PermanentUI.perm.Reset();
         }
         else if (PermanentUI.perm.LastScene == 2)
         {
-            Description.text = "The Objective of the Next Level is to Recruit All 15 New Members within 90 seconds";
+            Description.text = "Progress: Your local chapter needs more members!Recruit 15 members in 90 seconds to get to the next level";
             Requirement.text = "The required answers to access the next level is all 5";
             Current.text = "Your current score is " + PermanentUI.perm.points;
             PermanentUI.perm.Reset();
         }
         else if (PermanentUI.perm.LastScene == 3)
         {
-            Description.text = "Good job!     The Objective of the Next Level is to get to the end in 90 seconds";
+            Description.text = "Great Work! Get to the conference as fast as you can to claim your awards!";
             Requirement.text = "";
             Current.text = "You have " + PermanentUI.perm.points + " points";
             PermanentUI.perm.Reset();
